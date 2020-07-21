5a213bde78707c47589d78bbf5143d6a727755ed
louisej <louisejaq@gmail.com>
2020-07-21 14:42:16 -0400

    para abrir a branch
    troquei o m por n nos 20 mim

--- src/services/Authenticator.ts
+++ src/services/Authenticator.ts
@@ -1,32 +1,31 @@
-import * as jwt from 'jsonwebtoken'
+import * as jwt from "jsonwebtoken";
 
 interface AuthenticatorData {
-    id:string
+  id: string;
 }
 
-export class Authenticator{
-    private static EXPRESS_IN = "20mim"
+export class Authenticator {
+  private static EXPRESS_IN = "20min";
 
-    generateToken(input:AuthenticatorData):string{
-        const token = jwt.sign(
-            {
-                id:input.id,
-            },
-            process.env.JWT_KEY as string,
+  generateToken(input: AuthenticatorData): string {
+    const token = jwt.sign(
+      {
+        id: input.id,
+      },
+      process.env.JWT_KEY as string,
+      {
+        expiresIn: Authenticator.EXPRESS_IN,
+      },
+    );
 
-            {
-                expiresIn: Authenticator.EXPRESS_IN
-            }
-        )
+    return token;
+  }
 
-        return token
-    }
-
-    getData(token:string):AuthenticatorData{
-        const payload = jwt.verify(token, process.env.JWT_KEY as string) as any;
-        const result = {
-            id:payload.id
-        }
-        return result
-    }
+  getData(token: string): AuthenticatorData {
+    const payload = jwt.verify(token, process.env.JWT_KEY as string) as any;
+    const result = {
+      id: payload.id,
+    };
+    return result;
+  }
 }
